5cf4880658586e6bf6b0cfc9c804cd44257ec737
laratarpley13 <ltarp13@gmail.com>
2021-03-23 11:51:17 -0500

    edit to cors configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const usersRouter = require('./users/users-router')
@@ -17,7 +18,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
 
 app.use('/api/users', usersRouter)
 app.use('/api/lists', listsRouter)
