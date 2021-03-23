82ec9eee65250b581b3cc00e4fa3e33d869398bb
laratarpley13 <ltarp13@gmail.com>
2021-03-22 19:43:21 -0500

    added comma

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/list-manager',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/list-manager-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/list-manager-test',
     JWT_SECRET: process.env.JWT_SECRET || "list-manager-jwt",
   }
