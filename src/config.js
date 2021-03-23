e97dbc98ed070eaae70aa31d1c642ab3d32eca15
laratarpley13 <ltarp13@gmail.com>
2021-03-22 19:42:47 -0500

    edits to variable name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/list-manager',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/list-manager',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/list-manager-test'
     JWT_SECRET: process.env.JWT_SECRET || "list-manager-jwt",
   }
